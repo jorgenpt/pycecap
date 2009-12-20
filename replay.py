f5d277e828773e045c57993d68b536c253ddd9da
Jørgen P. Tjernø <jorgen@devsoft.no>
2009-12-19 20:13:38 -0800

    New replay client to replay logs by test.py

---
+++ replay.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python
+
+import client
+from sys import stdin
+
+from pprint import pprint
+
+c = client.Client()
+
+for line in stdin:
+    line = line.rstrip('\n\r')
+    dir, message = line[:1], line[1:]
+
+    if dir == '<':
+        c.parse(message)
+    else:
+        c.fakesend(message)
+
+pprint(c.__dict__)
