9460db76a153e12fdb942402d3d7911f91a1c6d5
Jørgen P. Tjernø <jorgen@devsoft.no>
2009-12-20 18:33:22 -0800

    Hierarchical state: Network -> LocalPresences -> Channels (-> Presences (should be under LocalPresence)). Ctors that take values from "old value".

--- state.py
+++ state.py
@@ -13,25 +13,39 @@
 # WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.
 
 class Network(dict):
-    def __init__(self, params={}, gateways=None):
+    def __init__(self, params={}, old_network=None):
         super(Network, self).__init__(params)
 
-        if gateways is None:
+        if old_network is None:
             self.gateways = []
+            self.presences = {}
         else:
-            self.gateways = gateways
+            self.gateways = old_network.gateways
+            self.presences = old_network.presences
 
     def __repr__(self):
-        return 'Network(params=%s, gateways=%r)' % (super(Network, self).__repr__(), self.gateways)
+        return 'Network(params=%s, gateways=%r, presences=%r)' % (super(Network, self).__repr__(), self.gateways, self.presences)
+
+class LocalPresence(dict):
+    def __init__(self, params={}, old_presence=None):
+        super(LocalPresence, self).__init__(params)
+
+        if old_presence is None:
+            self.channels = {}
+        else:
+            self.channels = old_presence.channels
+
+    def __repr__(self):
+        return 'LocalPresence(params=%s, channels=%r)' % (super(LocalPresence, self).__repr__(), self.channels)
 
 class Channel(dict):
-    def __init__(self, params={}, presences=None):
+    def __init__(self, params={}, old_network=None):
         super(Channel, self).__init__(params)
 
-        if presences is None:
+        if old_network is None:
             self.presences = []
         else:
-            self.presences = presences
+            self.presences = old_network.presences
 
     def __repr__(self):
         return 'Channel(params=%s, presences=%r)' % (super(Channel, self).__repr__(), self.presences)
