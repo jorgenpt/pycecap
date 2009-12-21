e4611a2fb76491105d28b96ce01b17bf99662589
Jørgen P. Tjernø <jorgen@devsoft.no>
2009-12-20 18:36:06 -0800

    Use the new state hierarchy, and also handle channel_presence_init and the reply to channel names.

--- client.py
+++ client.py
@@ -30,20 +30,46 @@ class Client(object):
             'network list': self.network_list,
             'gateway list': self.gateway_list,
             'presence list': self.presence_list,
-            'channel list': self.channel_list
+            'channel list': self.channel_list,
+            'channel names': self.channel_presence_list,
         }
 
         self._state_handlers = {
             'network_init': self.network_add,
             'gateway_init': self.gateway_add,
             'presence_init': self.presence_add,
-            'channel_init': self.channel_add
+            'channel_init': self.channel_add,
+            'channel_presence_init': self.channel_presence_add,
         }
 
         # Blank state.
         self._networks = {}
-        self._presences = []
-        self._channels = []
+
+    def get(self, network_or_params, presence=None, channel=None):
+        if isinstance(network_or_params, dict):
+            network = network_or_params['network']
+            presence = network_or_params.get('mypresence')
+            channel = network_or_params.get('channel')
+        else:
+            network = network_or_params
+
+        if network not in self._networks:
+            self._networks[network] = state.Network()
+        network = self._networks[network]
+
+        if presence is None:
+            return network
+
+        if presence not in network.presences:
+            network.presences[presence] = state.LocalPresence()
+        presence = network.presences[presence]
+
+        if channel is None:
+            return presence
+
+        if channel not in presence.channels:
+            presence.channels[channel] = state.Channel()
+        return presence.channels[channel]
 
     def presend(self, object_or_command, params=None):
         if isinstance(object_or_command, basestring):
@@ -83,69 +109,68 @@ class Client(object):
                 del self._pending_commands[reply.tag]
 
     def network_list(self, command):
-        print "network_list(%r)" % command
-
         new_networks = {}
         for reply in command.replies:
             if reply.command == reply.MORE:
                 network = reply.params['network']
-
-                gateways = []
-                if network in self._networks:
-                    gateways = self._networks[network].gateways
-
-                new_networks[network] = state.Network(reply.params, gateways)
+                new_networks[network] = state.Network(reply.params, self._networks.get(network))
 
         self._networks = new_networks
 
     def network_add(self, event):
-        print "network_add(%r)" % event
         network = event.params['network']
-
-        gateways = []
-        if network in self._networks:
-            gateways = self._networks[network].gateways
-
-        self._networks[network] = state.Network(event.params, gateways)
+        self._networks[network] = state.Network(event.params, self._networks.get(network))
 
     def gateway_list(self, command):
-        print "gateway_list(%r)" % command
         for network in self._networks.iterkeys():
             self._networks[network].gateways = []
 
         for reply in command.replies:
             if reply.command == reply.MORE:
                 gateway = reply.params
-                if gateway['network'] not in self._networks:
-                    self._networks[gateway['network']] = state.Network()
-                self._networks[gateway['network']].gateways.append(gateway)
+                self.get(gateway['network']).gateways.append(gateway)
 
     def gateway_add(self, event):
-        print "gateway_add(%r)" % event
         gateway = event.params
-        if gateway['network'] not in self._networks:
-            self._networks[gateway['network']] = state.Network()
-        self._networks[gateway['network']].gateways.append(gateway)
+        self.get(gateway['network']).gateways.append(gateway)
 
     def presence_list(self, command):
-        print "presence_list(%r)" % command
-        self._presences = []
         for reply in command.replies:
             if reply.command == reply.MORE:
-                self._presences.append(reply.params)
+                network, presence = reply.params['network'], reply.params['mypresence']
+                network = self.get(network)
+                network.presences[presence] = state.LocalPresence(reply.params, network.presences.get(presence))
 
     def presence_add(self, event):
-        print "presence_add(%r)" % event
-        self._presences.append(event.params)
+        if event.get('own'):
+            network, presence = reply.params['network'], reply.params['presence']
+            network = self.get(network)
+            network.presences[presence] = state.LocalPresence(reply.params, network.presences.get(presence))
 
     def channel_list(self, command):
-        print "channel_list(%r)" % command
-        self._channels = []
+        presence = self.get(command.params)
+        new_channels = {}
+
         for reply in command.replies:
             if reply.command == reply.MORE:
-                self._channels.append(reply.params)
+                channel = reply['channel']
+                new_channels[channel] = state.Channel(reply.params, presence.channels.get(channel))
+
+        presence.channels = new_channels
 
     def channel_add(self, event):
-        print "channel_add(%r)" % event
-        self._channels.append(event.params)
+        network, presence, channel = event.params['network'], event.params['mypresence'], event.params['channel']
+        presence = self.get(network, presence)
 
+        presence.channels[channel] = state.Channel(event.params, presence.channels.get(channel))
+
+    def channel_presence_list(self, command):
+        channel = self.get(command.params)
+        channel.presences = []
+        for reply in command.replies:
+            if reply.command == reply.MORE:
+                channel.presences.append(reply.params)
+
+    def channel_presence_add(self, event):
+        channel = self.get(event.params)
+        channel.presences.append(event.params)
