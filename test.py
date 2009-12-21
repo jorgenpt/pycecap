ba5b570ce876c4135ca65edb37f2c6a12ea6d8c8
Jørgen P. Tjernø <jorgen@devsoft.no>
2009-12-20 17:08:13 -0800

    Print name of replay-log when quitting.

--- test.py
+++ test.py
@@ -12,7 +12,9 @@ import select
 import subprocess
 from sys import stdin
 
-def doWork(c, icecap, replay):
+log_name = 'logs/replay-%i.log' % os.getpid()
+
+def work(c, icecap, replay):
     rlist = [stdin, icecap.stdout]
     try:
         ready, _, _ = select.select(rlist, [], [])
@@ -47,12 +49,13 @@ def doWork(c, icecap, replay):
 c = client.Client()
 icecap = subprocess.Popen(['ssh', 'arach', 'icecapd'], stdin=subprocess.PIPE, stdout=subprocess.PIPE)
 
-if not os.path.isdir('logs'):
-    os.mkdir('logs')
-replay = open('logs/replay-%i.log' % os.getpid(), 'w')
+dir_name = os.path.dirname(log_name)
+if not os.path.isdir(dir_name):
+    os.makedirs(dir_name)
+replay = open(log_name, 'w')
 
 try:
-    while doWork(c, icecap, replay):
+    while work(c, icecap, replay):
         pass
 except Exception, e:
     print e
@@ -60,3 +63,4 @@ finally:
     replay.close()
     icecap.terminate()
     pprint(c.__dict__)
+    print 'Wrote replaylog to %s' % log_name
