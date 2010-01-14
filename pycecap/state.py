036c8475d54e80e94a54bc77c9d94db5633e457a
Jørgen P. Tjernø <jorgen@devsoft.no>
2010-01-14 12:08:34 -0800

    Docstrings for modules, classes and __init__s.

--- pycecap/state.py
+++ pycecap/state.py
@@ -12,10 +12,22 @@
 # IMPLIED WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED
 # WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.
 
+'''A set of classes to represent the state of an icecap session.'''
+
 import weakref
 
 class Network(object):
+    '''This is just a name and a set of gateways to connect to that network.'''
+
     def __init__(self, network, info, old_me=None):
+        '''Create a new network.
+
+        Args:
+            network: Name of the network.
+            info: Dict of arbitrary information about the network (from e.g. a 'network list' reply)
+            old_me: Optional argument, if present - should be a Network that we copy gateways from.
+        '''
+
         self.network = network
         self.info = info
 
@@ -28,7 +40,17 @@ class Network(object):
         return 'Network(%r, %r, <gateways=%r>)' % (self.network, self.info, self.gateways)
 
 class Connection(object):
+    '''This is a (unique) mypresence/network name pair (identifier) representing a connection to a service (e.g. IRC).'''
+
     def __init__(self, network_or_dict, mypresence=None):
+        '''Create a new connection identifier.
+
+        Args:
+            network_or_dict: Name of the network, or if mypresence is None, a dict containing
+                the 'network' and 'mypresence' keys.
+            mypresence: Optional argument, if present - should be the name of the local presence.
+        '''
+
         if mypresence is None:
             self.network = network_or_dict.pop('network')
             self.mypresence = network_or_dict.pop('mypresence')
@@ -48,7 +70,18 @@ class Connection(object):
         return 'Connection(%r, %r)' % (self.network, self.mypresence)
 
 class LocalPresence(object):
+    '''This is all the information associated with a Connection.'''
+
     def __init__(self, connection, info, old_me=None):
+        '''Create a new local presence.
+
+        Args:
+            connection: A Connection object identifying this LocalPresence
+            info: Dict of arbitrary information about the network (from e.g. a 'presence list' reply)
+            old_me: Optional argument, if present - should be a LocalPresence that we copy
+                channels & presences from.
+        '''
+
         self.connection = connection
         self.info = info
 
@@ -73,7 +106,22 @@ class LocalPresence(object):
         return 'LocalPresence(%r, %r, <channels=%r, presences=%r>)' % (self.connection, self.info, self.channels, self.presences)
 
 class LocalPresenceObject(object):
+    '''This represents some kind of data associated with a LocalPresence.
+
+    LocalPresenceObject is the parent class for classes that represent information
+    that's associated with one (and only one) LocalPresence (Connection). It takes
+    care of knowing which LocalPresence that is, and reparenting if needed.
+    '''
+
     def __init__(self, local_presence, name, info):
+        '''Create a new local presence info blob.
+
+        Args:
+            local_presence: The LocalPresence we belong to.
+            name: The name of this info blob.
+            info: Dict of arbitrary information about the network (from e.g. a 'presence list' reply)
+        '''
+
         self.local_presence = weakref.ref(local_presence)
         self.name = name
         self.info = info
@@ -82,7 +130,18 @@ class LocalPresenceObject(object):
         self.local_presence = weakref.ref(new_lp)
 
 class Presence(LocalPresenceObject):
+    '''This represents a presence (self or someone else) that we've been told about, and information about it.'''
+
     def __init__(self, local_presence, name, info, old_me=None):
+        '''Create a new presence info blob.
+
+        Args:
+            local_presence: The LocalPresence we belong to.
+            name: The name of this presence.
+            info: Dict of arbitrary information about the network (from e.g. a 'presence list' reply)
+            old_me: Optional argument, if present - should be a Presence that we copy channels from.
+        '''
+
         super(Presence, self).__init__(local_presence, name, info)
 
         if old_me is not None:
@@ -94,7 +153,18 @@ class Presence(LocalPresenceObject):
         return 'Presence(%r, %r, %r, <channels=%r>)' % (self.local_presence().connection, self.name, self.info, self.channels)
 
 class Channel(LocalPresenceObject):
+    '''This represents a channel that we've been told about, and information about it.'''
+
     def __init__(self, local_presence, name, info, old_me=None):
+        '''Create a new channel info blob.
+
+        Args:
+            local_presence: The LocalPresence we belong to.
+            name: The name of this channel.
+            info: Dict of arbitrary information about the network (from e.g. a 'presence list' reply)
+            old_me: Optional argument, if present - should be a Channel that we copy presences from.
+        '''
+
         super(Channel, self).__init__(local_presence, name, info)
 
         if old_me is not None:
